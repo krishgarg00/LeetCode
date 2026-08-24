86943a7b4c7bfe3a7853212ef42f65dfece7091c
Krish Garg <krishgarg712@gmail.com>
2026-08-24 17:16:47 +0530

    Time: 0 ms (100%), Space: 42.8 MB (83.91%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String toLowerCase(String s) {
+        // String str="";
+        // for(int i=0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     if(ch>=65 && ch<=96){
+        //         str+=(char)(ch+32);
+        //     }
+        //     else if(ch>=97 && ch<=122){
+        //         str+=ch;
+        //     }
+        //     else{
+        //         str+=ch;
+        //     }
+        // }
+        return s.toLowerCase();
+    }
+}
